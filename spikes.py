9d1b4b7bdf5832d87aaf7a6125b8a6c6d8f11aaf
JasonCRodriguez <jasoncrodriguez@gmail.com>
2014-03-10 15:45:31 -0400

    eliminated extra files

--- spikes.py
+++ spikes.py
@@ -1,15 +1,19 @@
 '''
 Created on Feb 24, 2014
 
-@author: Jason C Rodriguez
+@author: Jason C Rodriguez, Gabriel F Colton
 '''
 import pandas as pd
 import numpy as np
 
-class SpikeTimeSeries(object):
+class SpikeDataFrame(object):
     
-    def __init__(self, time_series):
-        self.time_series = pd.Series(time_series)
+    
+    
+    def __init__(self, time_array, voltage_array):
+        self.time_voltage_df = pd.DataFrame({'time': time_array, 'voltage': voltage_array})
+        # DataFrame will normally consist of a file/CSV/spreadsheet 
+        # two columns, t, V
         
     
     def thresholdArray(self, input_threshold):
@@ -18,7 +22,7 @@ class SpikeTimeSeries(object):
         that are above input_threshold 
         '''
         self.threshold = input_threshold
-        self.threshold_series = self.time_series > self.threshold
+        self.threshold_series = self.time_voltage_df.loc[:,'voltage'] > self.threshold
         return self.threshold_series.astype(int)
                     
     def spikeArray(self):
@@ -26,6 +30,7 @@ class SpikeTimeSeries(object):
         converts a time series into a numpy boolean array with 
         True's for all threshold crossings
         '''
+        
         # converts the time series produced by thresholdArray() into a numpy array
         int_array = np.array(self.thresholdArray(self.threshold).values)
         #makes a new array that is shifted one spot 
@@ -45,16 +50,28 @@ class SpikeTimeSeries(object):
         return thresh_cross_bool_array
       
     def threshCrossingsIndicesArray(self):
-        start_point = self.time_series.iloc[1] > self.threshold
-        #self.starting_point_relative_to_threshold = self.time_series.iloc[1] > self.threshold
+        
+        
+        starting_point_relative_to_threshold = self.time_voltage_df.loc[1, 'voltage'] > self.threshold
+    	print starting_point_relative_to_threshold
     	#true - starts above threshold, false - starts below threshold 
-    	bool = self.spikeArray()
-    	spike_array_length = bool.size
-     	print spike_array_length
-     	
-    	even = np.array(range(1,spike_array_length, 2))
-    	odd = np.array(range(0,spike_array_length, 2))
-    	thresh_crossing_indices = np.where(start_point, even, odd) 
+    	bool = np.delete(self.spikeArray(),-1)
+    	print bool
+        all_thresh_cross = self.time_voltage_df.loc[bool, 'time']
+        print all_thresh_cross
+        len = all_thresh_cross.count()
+        print len
+    	if starting_point_relative_to_threshold == True:
+    		return all_thresh_cross.iloc[2:len:2]
+    	else:
+    	    return all_thresh_cross.iloc[1:len:2]
+    	
+    	# spike_array_length = bool.size
+#      	print spike_array_length
+#      	
+#     	even = np.array(range(1,spike_array_length, 2))
+#     	odd = np.array(range(0,spike_array_length, 2))
+#     	thresh_crossing_indices = np.where(start_point, even, odd) 
     	# if self.starting_point_relative_to_threshold == true: #starts above threshold
 # 	        even = np.array(range(1,spike_array_length, 2)   
 # 	    else:
--- tests/spikes_test.py
+++ tests/spikes_test.py
@@ -1,9 +1,9 @@
 '''
 Created on Feb 24, 2014
 
-@author: Jason C Rodriguez
+@author: Jason C Rodriguez, Gabriel F Colton
 '''
-import spikes
+import spikes as sp
 import unittest
 
 
@@ -11,18 +11,34 @@ class TestSpikesFunctions(unittest.TestCase):
 
 
     def setUp(self):
-        self.series = range(10)
-        self.threshold = int(5)
+        self.t_array = range(16) 
+        self.v_array = [0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0, 
+                            1, 2, 3, 4, 5]
+        self.threshold = int(3)
 
     def tearDown(self):
         pass
 
-    def testSpikes(self):
-        obj = spikes.SpikeTimeSeries(self.series)
+    def testthresholdArray(self):
+        obj = sp.SpikeDataFrame(self.t_array, self.v_array)
         threshold_series = obj.thresholdArray(self.threshold)
-        self.failUnless(list(threshold_series) == [0,0,0,0,0,0,1,1,1,1])
-
-      
+        self.failUnless(list(threshold_series) == [0,0,0,0,1,1,1,0,0,0,0,0,0,0,1,1])
+       
+    def testspikeArrayTest(self):
+        obj = sp.SpikeDataFrame(self.t_array, self.v_array)
+        threshold_series = obj.thresholdArray(self.threshold)
+        boolean_spike_array = obj.spikeArray()
+        self.failUnless(list(boolean_spike_array) == [True, False, False, False, True, 
+            False, False, True, False, False, False, False, False, False, True, 
+            False, True])
+    
+    def testthreshCrossingsIndicesArrayTest(self):
+        obj = sp.SpikeDataFrame(self.t_array, self.v_array)
+        threshold_series = obj.thresholdArray(self.threshold)
+        index_array = obj.threshCrossingsIndicesArray()
+        self.failUnless(list(index_array) == [4, 14])
+   
+  
 def main():
     unittest.main()
     
